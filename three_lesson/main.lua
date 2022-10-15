89fa7f57fb876493c9e3ee6e881d58fad89f21d9
Kalinka George <xitowzys@gmail.com>
2022-10-15 15:33:01 +0800

    ✨ feat: Squares changing the angle of collision (15.10.2022)

--- three_lesson/main.lua
+++ three_lesson/main.lua
@@ -3,50 +3,25 @@ require("mover")
 require("attractor")
 
 function love.load()
-    movers = {}
-
     width = love.graphics.getWidth()
     height = love.graphics.getHeight()
-    G = 2.0
-    local location = Vector:create(width / 2 + 100, height / 2)
 
-    attractor = Attractor:create(location, 20)
+    mover = Mover:random()
+    mover2 = Mover:random()
 
-    local location = Vector:create(300, 350)
-    local velocity = Vector:create(0.1, 0.1)
-    mover = Mover:create(location, velocity)
-
-    for i = 0, 9, 1 do
-        local location = Vector:create(love.math.random(300, 350), love.math.random(300, 350))
-        local velocity = Vector:create(love.math.random(0.0, 3), love.math.random(0.0, 3))
-        movers[i] = Mover:create(location, velocity)
-    end
-
+    mover.aVelocity = 0.2
+    mover2.aVelocity = -0.3
 end
 
 function love.update(dt)
-    -- mover:update()
-    -- attractor:attract(mover)
+    mover:update()
+    mover:checkBoundaries()
 
-    for i = 0, 9, 1 do
-        movers[i]:update()
-        movers[i]:checkBoundaries()
-        for j = 0, 9, 1 do
-            movers[i]:attract(movers[j])
-        end
-    end
-
-
+    mover2:update()
+    mover2:checkBoundaries()
 end
 
 function love.draw()
-    -- mover:draw()
-    -- attractor:draw()
-
-    for i = 0, 9, 1 do
-        movers[i]:draw()
-    end
-end
-
-function love.keypressed(key)
+    mover:draw()
+    mover2:draw()
 end
--- three_lesson/mover.lua
+++ three_lesson/mover.lua
@@ -6,10 +6,11 @@ function Mover:create(location, velocity, weight)
     setmetatable(mover, Mover)
     mover.location = location
     mover.velocity = velocity
+    mover.aVelocity = 0
+    mover.angle = 0
     mover.acceleration = Vector:create(0, 0)
     mover.weight = weight or 1
     mover.size = 20 * mover.weight
-    mover.attractor = Attractor:create(location, mover.weight)
     return mover
 end
 
@@ -20,11 +21,16 @@ function Mover:random()
     local velocity = Vector:create()
     velocity.x = love.math.random(-2, 2)
     velocity.y = love.math.random(-2, 2)
-    return Mover:create(location, velocity)
+    local weight = love.math.random(1, 4)
+    return Mover:create(location, velocity, weight)
 end
 
 function Mover:draw()
-    love.graphics.circle("fill", self.location.x, self.location.y, self.size)
+    love.graphics.push()
+    love.graphics.translate(self.location.x, self.location.y)
+    love.graphics.rotate(self.angle)
+    love.graphics.rectangle("fill", -self.size / 2, -self.size / 2, self.size, self.size)
+    love.graphics.pop()
 end
 
 function Mover:applyForce(force)
@@ -53,9 +59,6 @@ end
 function Mover:update()
     self.velocity = self.velocity + self.acceleration
     self.location = self.location + self.velocity
+    self.angle = self.angle + self.aVelocity
     self.acceleration:mul(0)
-end
-
-function Mover:attract(mover)
-    self.attractor:attract(mover)
 end
